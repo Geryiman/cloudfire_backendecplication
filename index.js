0b6cad0e45ab83aa06622d8ca4f79605ca77747b
Geryiman <geryiman0@gmail.com>
2025-03-23 21:41:28 -0600

    final del primer backend modioficado

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const routes = require('./src/routes/index');
+const port = 3000;
+const { init } = require('./src/services/services_locator/composer');
+
+// Registramos todas las dependencias que necesitamos (Cloudflare, etc.)
+init();
+
+// Configuramos nuestras rutas después de tener las dependencias listas
+routes(app);
+
+// Lanzamos el servidor en el puerto definido
+app.listen(port, () => {
+    console.log(`🚀 Servidor escuchando en el puerto ${port}`);
+});
